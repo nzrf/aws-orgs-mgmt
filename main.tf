832bb047af8f002dab8ff8c80bf7542420836ce1
Dylan <dylan.hager@gmail.com>
2021-06-28 16:07:34 -0500

    added some test scp's and changed ou to own file

--- main.tf
+++ main.tf
@@ -12,29 +12,7 @@ resource "aws_organizations_organization" "org" {
   ]
 }
 
-#Setting up application top level org ou. Need to figure out how to make this a module.
-resource "aws_organizations_organizational_unit" "application" {
-    # OU Must be created before childen n the ou can be created
-    depends_on = [aws_organizations_organization.org]
-    name =  "application"
-    parent_id = aws_organizations_organization.org.roots[0].id
-}
 
-#Setting up infrastructure top level org ou. Need to figure out how to make this a module.
-resource "aws_organizations_organizational_unit" "infrastructure" {
-    # OU Must be created before childen n the ou can be created
-    depends_on = [aws_organizations_organization.org]
-    name =  "infrastructure"
-    parent_id = aws_organizations_organization.org.roots[0].id
-}
-
-#Setting up nonprod org unit under application. Need to figure out how to make this more clear or a module.
-resource "aws_organizations_organizational_unit" "app_nonprod" {
-    # OU Must be created before childen n the ou can be created
-    depends_on = [aws_organizations_organizational_unit.application]
-    name =  "nonprod"
-    parent_id = aws_organizations_organizational_unit.application.id
-}
 
 
 module "aws_org_scps" {
--- modules/aws_org_scps/variables.tf
+++ modules/aws_org_scps/variables.tf
@@ -1,11 +1,22 @@
 variable "require_s3_encryption_target_ids" {
-  description = "Target ids (AWS Account or Organizational Unit) to attach an SCP requiring S3 encryption"
+  description = "Target ids (Organizational Unit) to attach an SCP requiring S3 encryption"
   type        = list(string)
   default     = []
 }
 
 variable "ai_data_services_optout_ids" {
-  description = "Target ids (AWS Account or Organizational Unit) to attach an SCP requiring S3 encryption"
+  description = "Target ids (Organizational Unit) to attach an SCP requiring S3 encryption"
+  type        = list(string)
+  default     = []
+}
+
+variable "deny_leaving_org_target_ids" {
+  description = "Target ids (Organizational Unit) to attach an SCP to deny leaving org"
+  type        = list(string)
+  default     = []
+}
+variable "limit_aws_regions_target_ids" {
+  description = "Target ids (Organizational Unit) to attach an SCP to deny services outside us-east-1 and us-east-2"
   type        = list(string)
   default     = []
 }
