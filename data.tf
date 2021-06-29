63ad5c96efe4c4327582bcf6429c0d15bbbeaa46
Dylan <dylan.hager@gmail.com>
2021-06-29 12:03:00 -0500

    clean up more files

--- data.tf
+++ data.tf
@@ -4,7 +4,3 @@ data "aws_organizations_organization" "org" {
 data "aws_organizations_organizational_units" "ou" {
   parent_id = data.aws_organizations_organization.org.roots[0].id
 }
-
-data "aws_organizations_organization" "testacct" {
-  accounts_id = data.aws_organizations_organization.org.accounts
-}
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "require_s3_encryption_target_ids" {
-  description = "Target ids (AWS Account or Organizational Unit) to attach an SCP requiring S3 encryption"
-  type        = list(string)
-  default     = []
-}
